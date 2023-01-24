feb05b888b01c3ee1cc83cc97b7548de9cd26695
Sonam Singh <sonamstkarens99@gmail.com>
2023-01-24 15:41:30 +0530

    Added Jacoco

--- HotelService 2/src/test/java/com/HotelServices/service/HotelServiceTest.java
+++ HotelService 2/src/test/java/com/HotelServices/service/HotelServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,7 +35,7 @@ class HotelServiceTest {
         Hotel hotel1 = hotelService.create(hotel);
         assertNotNull(hotel1);
         //Negative Scenario
-        assertEquals("abc", hotel1.getName());
+        assertEquals("hotel 1", hotel1.getName());
     }
     @Test
     void getHotelById() {
@@ -42,14 +44,33 @@ class HotelServiceTest {
         hotel.setName("hotel 1");
         hotel.setAbout("good");
         hotel.setLocation("location 1");
-        String id = "2";
+        String id = "1";
         when(hotelRepo.findById(id)).thenReturn(Optional.of(hotel));
         Hotel hotel1 = hotelService.getIdHotel(id);
         assertNotNull(hotel1);
         assertEquals(id, hotel1.getId());
 
     }
+    @Test
+    void getAllHotel() {
+        Hotel hotel = new Hotel();
+        hotel.setId("1");
+        hotel.setName("hotel 1");
+        hotel.setAbout("good");
+        hotel.setLocation("location 1");
 
+        Hotel hotel1 = new Hotel();
+        hotel1.setId("2");
+        hotel1.setName("hotel 2");
+        hotel1.setAbout("good");
+        hotel1.setLocation("location 2");
+        List<Hotel> hotelList = new ArrayList<>();
+        hotelList.add(hotel);
+        hotelList.add(hotel1);
+        when(hotelRepo.findAll()).thenReturn(hotelList);
+        List<Hotel> hotels = hotelService.getAllHotel();
+        assertEquals(hotelList.size(), hotels.size());
+    }
 
 
 }
